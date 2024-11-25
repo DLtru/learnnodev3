116fc125b3d3c2dcdea0af654330d5d524bccaa4
Dimitri Larionov <dimitri.larionov@tptlive.ee>
2024-11-25 11:23:08 +0200

    Add nunjucks

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import path from "path";
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import loader from "sass-loader";
 
 const __filename = import.meta.filename;
 const __dirname = import.meta.dirname;
@@ -38,14 +39,23 @@ export default {
                 }
             ],
           },
-        ],
-    },
+          {
+          test: /\.njk$/,
+          use: [
+            {
+              loader: "simple-nunjucks-loader",
+              options: {},
+            },
+          ]
+        }
+    ],
+},
     plugins: [
         new HtmlWebpackPlugin({
-            template:  './src/index.html',
+            template:  './src/index.njk',
         }),
         new HtmlWebpackPlugin({
-            template:  './src/index.html',
+            template:  './src/about.njk',
             filename: 'about.html'
         }),
     ],
