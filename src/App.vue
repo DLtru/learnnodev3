0e9568f7ef8436fbe10dee99e51df250eaf96270
Dimitri Larionov <dimitri.larionov@tptlive.ee>
2025-02-11 13:01:26 +0200

    set timeout example

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
-let message = 'Hello Vue!'
+import { ref } from 'vue';
+
+let message = ref('Hello Vue!');
 </script>
 <template>
     <div class="container">
         <div class="content">
+            <button class="button is-primary">Click me!</button>
             <h1>{{ message }}</h1>
         </div>
     </div>
 </template>
 <style>
-
 </style>
