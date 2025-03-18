32b58ec16da9eb3e0a20041021f8d1f0723866ef
Dimitri Larionov <dimitri.larionov@tptlive.ee>
2025-03-18 13:44:51 +0200

    Add cookie clicker

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import "./style.scss";
+import './style.scss';
 
-import { createApp } from "vue";
-import { createWebHashHistory, createWebHistory, createMemoryHistory, createRouter } from 'vue-router'
+import { createApp } from 'vue';
+import { createWebHashHistory, createWebHistory, createMemoryHistory, createRouter } from 'vue-router';
 
-import App from './App.vue'
+import App from './App.vue';
 
 import ToDo from './pages/ToDo.vue';
 import Modals from './pages/Modals.vue';
 import ChuckNorris from './pages/ChuckNorris.vue';
-import RickAndMorty from "./pages/RickAndMorty.vue";
-import CookieClicker from './pages/CookieClicker.vue'
+import RickAndMorty from './pages/RickAndMorty.vue';
+import CookieClicker from './pages/CookieClicker.vue';
 
 const routes = [
   { path: '/', component: ToDo, name: 'ToDo' },
   { path: '/modals', component: Modals, name: 'Modals' },
   { path: '/chuck', component: ChuckNorris, name: 'Chuck Norris' },
-  { path: '/rickandmorty', component: RickAndMorty, name: 'Rick And Morty'},
-  { path: '/cookieclicker', component: CookieClicker, name: 'Cookie Clicker'}
+  { path: '/rickandmorty', component: RickAndMorty, name: 'Rick And Morty' },
+  { path: '/cookieclicker', component: CookieClicker, name: 'Cookie Clicker', meta: { container: false } },
 ];
 
 const router = createRouter({
@@ -26,5 +26,5 @@ const router = createRouter({
 
 
 const app = createApp(App);
-app.use(router)
-app.mount('#app')
+app.use(router);
+app.mount('#app');
