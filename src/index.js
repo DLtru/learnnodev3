694c6efa19b49ed14050ecce77f6625dda2d3164
Dimitri Larionov <dimitri.larionov@tptlive.ee>
2025-04-01 12:36:37 +0300

    Add webapi example on vibrate

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import './style.scss';
+import "./style.scss";
 
-import { createApp } from 'vue';
-import { createWebHashHistory, createWebHistory, createMemoryHistory, createRouter } from 'vue-router';
+import { createApp } from "vue";
+import { createWebHashHistory, createWebHistory, createMemoryHistory, createRouter } from 'vue-router'
 
-import App from './App.vue';
+import App from './App.vue'
 
 import ToDo from './pages/ToDo.vue';
 import Modals from './pages/Modals.vue';
 import ChuckNorris from './pages/ChuckNorris.vue';
-import RickAndMorty from './pages/RickAndMorty.vue';
+import RickAndMorty from "./pages/RickAndMorty.vue";
 import CookieClicker from './pages/CookieClicker.vue';
+import Vibration from "./pages/Vibration.vue";
 
 const routes = [
   { path: '/', component: ToDo, name: 'ToDo' },
   { path: '/modals', component: Modals, name: 'Modals' },
   { path: '/chuck', component: ChuckNorris, name: 'Chuck Norris' },
-  { path: '/rickandmorty', component: RickAndMorty, name: 'Rick And Morty' },
+  { path: '/rickandmorty', component: RickAndMorty, name: 'Rick And Morty'},
   { path: '/cookieclicker', component: CookieClicker, name: 'Cookie Clicker', meta: { container: false } },
+  { path: '/vibration', component: Vibration, name: 'Vibration'},
 ];
 
 const router = createRouter({
@@ -26,5 +28,5 @@ const router = createRouter({
 
 
 const app = createApp(App);
-app.use(router);
-app.mount('#app');
+app.use(router)
+app.mount('#app')
