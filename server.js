17be12574d57457697ef5143747fafcf432b3caf
Dimitri Larionov <dimitri.larionov@tptlive.ee>
2025-04-15 12:50:09 +0300

    Fix server sending all messages all the time

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 import express from 'express';
- import bodyParser from 'body-parser';
- const app = express();
- const port = 3000;
- 
- let messages = [];
- 
- app.use(bodyParser.json());
- 
- app.use((req, res, next) => {
-     res.set('Access-Control-Allow-Origin', '*');
-     res.set('Access-Control-Allow-Headers', 'Content-Type');
-     next();
- });
- 
- app.get('/messages', (req, res) => {
-   res.json(messages);
- });
- 
- 
- app.post('/messages', (req, res) => {
-     messages.push(req.body);
-     res.json(req.body);
- });
-   
- 
- 
- app.listen(port, () => {
-   console.log(`Example app listening on port http://localhost:${port}`);
- });
+import bodyParser from 'body-parser';
+const app = express();
+const port = 3000;
+
+let messages = [];
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.get('/messages', (req, res) => {
+  let filteredMessages = messages.filter(msg => msg.date > new Date(req.query.date ?? null));
+     res.json(filteredMessages);
+});
+
+
+app.post('/messages', (req, res) => {
+  messages.push({ message: req.body.message, date: new Date()});
+    res.json(req.body);
+});
+  
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
